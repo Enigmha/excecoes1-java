2aed60371a4c87a349e2bdb2ba573f686191cee6
Leandro Cabral <enigmha@gmail.com>
2020-07-19 12:04:18 -0300

    solucao ruim

---
+++ src/aplicacao/ProgramaEx.java
@@ -0,0 +1,60 @@
+package aplicacao;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entidade.Reserva;
+
+public class ProgramaEx {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf =new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Numero do Quarto: ");
+		Integer numQuarto = sc.nextInt();
+		System.out.print("Check-IN data (DD/MM/AAAA): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Check-Out data (DD/MM/AAAA): ");
+		Date checkOut = sdf.parse(sc.next());
+		
+		if(!checkOut.after(checkIn)) {
+			System.out.println("Erro na reserva: Check-Out data maior que a data Check-In ");
+		}
+		else {
+			Reserva reserva = new Reserva(numQuarto, checkIn, checkOut);
+			System.out.println("Reserva: "+ reserva );
+			
+		}
+		
+		System.out.println();
+		System.out.println("entra com dados para atualizar a reserva:");
+		
+		System.out.print("Check-IN data (DD/MM/AAAA): ");
+		checkIn = sdf.parse(sc.next());
+		System.out.print("Check-Out data (DD/MM/AAAA): ");
+		checkOut = sdf.parse(sc.next());
+		
+		Reserva reserva = new Reserva(numQuarto, checkIn, checkOut);
+		
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			System.out.println("As data te que ser futuras a data checkin");
+		}
+		else if(!checkOut.after(checkIn))  {
+			System.out.println("Erro na reserva: Check-Out data maior que a data Check-In ");
+		}
+		else {
+			reserva.updateDates(checkIn, checkOut);
+			System.out.println("Reserva: "+ reserva );
+		}
+		
+		
+		sc.close();
+
+	}
+
+}
